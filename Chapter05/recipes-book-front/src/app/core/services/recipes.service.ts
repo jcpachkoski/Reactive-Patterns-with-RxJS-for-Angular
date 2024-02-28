aa26fc14da0f9617e2766483b43cec2bc7d42bfa
John Pachkoski <jpachkoski@gmail.com>
2024-02-27 19:26:47 -0500

    Fixed catchError

--- Chapter05/recipes-book-front/src/app/core/services/recipes.service.ts
+++ Chapter05/recipes-book-front/src/app/core/services/recipes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from '../model/recipe.model';
 import { environment } from 'src/environments/environment';
-import { catchError, of } from 'rxjs';
+import { catchError, of } from 'rxjs/operators';
 const BASE_PATH = environment.basePath
 
 @Injectable({
@@ -10,7 +10,7 @@ const BASE_PATH = environment.basePath
 })
 
 export class RecipesService {
-  
+
   constructor(private http: HttpClient) { }
 
   recipes$ = this.http.get<Recipe[]>(`${BASE_PATH}/recipes`).pipe(
