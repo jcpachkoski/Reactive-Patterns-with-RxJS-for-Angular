baf82ed9bcf4c9882e36d6da3ef065ed4ceaeaac
John Pachkoski <jpachkoski@gmail.com>
2024-02-28 00:36:14 -0500

    Added extra line

--- Chapter06/recipes-book-front/src/app/app.module.ts
+++ Chapter06/recipes-book-front/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { HeaderComponent } from './core/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
