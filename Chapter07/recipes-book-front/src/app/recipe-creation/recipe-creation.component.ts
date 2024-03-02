8496b534296b361c3dd3f27f4c8391468101b8d2
John Pachkoski <jpachkoski@gmail.com>
2024-03-02 18:51:56 -0500

    Added debounceTime

--- Chapter07/recipes-book-front/src/app/recipe-creation/recipe-creation.component.ts
+++ Chapter07/recipes-book-front/src/app/recipe-creation/recipe-creation.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { RecipesService } from '../core/services/recipes.service';
 import * as recipeTags from '../core/model/tags';
-import { catchError, concatMap, tap } from 'rxjs/operators';
+import { catchError, concatMap, debounceTime, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 
 @Component({
@@ -12,6 +12,8 @@ import { of } from 'rxjs';
 export class RecipeCreationComponent implements OnInit{
 
   constructor(private formBuilder: FormBuilder, private service: RecipesService) { }
+
+  saveDebounceTime = 450;
 
   ngOnInit(): void {
   }
@@ -29,8 +31,9 @@ export class RecipeCreationComponent implements OnInit{
   });
 
   tags = recipeTags.TAGS;
-  
+
   valueChanges$ = this.recipeForm.valueChanges.pipe(
+    debounceTime(this.saveDebounceTime),
     concatMap(formValue => this.service.saveRecipe(formValue)),
     catchError(errors => of(errors)),
     tap(result=>this.saveSuccess(result))
