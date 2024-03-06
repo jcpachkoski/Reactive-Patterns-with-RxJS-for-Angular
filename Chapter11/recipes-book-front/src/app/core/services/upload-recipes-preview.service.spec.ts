2aa6eb7007e8cacb968c8985b3529e0635dd6896
John Pachkoski <jpachkoski@gmail.com>
2024-03-05 19:04:47 -0500

    Minor formatting

--- Chapter11/recipes-book-front/src/app/core/services/upload-recipes-preview.service.spec.ts
+++ Chapter11/recipes-book-front/src/app/core/services/upload-recipes-preview.service.spec.ts
@@ -1,5 +1,4 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UploadRecipesPreviewService } from './upload-recipes-preview.service';
 
 describe('UploadRecipesPreviewService', () => {
--- Chapter11/recipes-book-front/src/app/core/services/upload-recipes-preview.service.ts
+++ Chapter11/recipes-book-front/src/app/core/services/upload-recipes-preview.service.ts
@@ -11,16 +11,16 @@ const BASE_PATH = environment.basePath
 export class UploadRecipesPreviewService {
 
   constructor(private http: HttpClient) { }
-  
+
   /**
    * Uploads the file
-   * @param code 
-   * @param fileToUpload 
-   * @returns 
+   * @param code
+   * @param fileToUpload
+   * @returns
    */
   upload(code: string, fileToUpload?: File): Observable<UploadStatus> {
     const formData = new FormData()
     formData.append('fileToUpload', fileToUpload as File)
-    return this.http.post<UploadStatus>(`${BASE_PATH}/recipes/upload/${code}`,formData)
+    return this.http.post<UploadStatus>(`${BASE_PATH}/recipes/upload/${code}`, formData)
   }
 }
