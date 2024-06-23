8e7c11c2eaab618635665994bb60ed2c9494ae17
John Pachkoski <jpachkoski@gmail.com>
2024-06-22 20:47:02 -0400

    Modified home component

--- Chapter06/recipes-book-front/src/app/app-routing.module.ts
+++ Chapter06/recipes-book-front/src/app/app-routing.module.ts
@@ -4,7 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-//  { path: '', component: HomeComponent  }
+  // { path: '', component: HomeComponent  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
--- Chapter06/recipes-book-front/src/app/app.module.ts
+++ Chapter06/recipes-book-front/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app.component';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
 import { HeaderComponent } from './core/header/header.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RecipesService } from './core/services/recipes.service';
+// import { RecipesService } from './core/services/recipes.service';
 import {DataViewModule} from 'primeng/dataview';
 import {ButtonModule} from 'primeng/button';
 import {PanelModule} from 'primeng/panel';
@@ -43,7 +43,8 @@ import { AppRoutingModule } from './app-routing.module';
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [RecipesService],
+  // providers: [RecipesService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
